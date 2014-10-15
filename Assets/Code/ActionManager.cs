a8c731eab84c76a3280a29d1b4bbd536cb8acca8
Nrick <8320159@radford.act.edu.au>
2014-10-15 12:26:54 +1100

    dictionary addding same key issue

--- Assets/Code/ActionManager.cs
+++ Assets/Code/ActionManager.cs
@@ -567,6 +567,8 @@ public class ActionManager {
 
 	private void makePrevLoS () //created by Nick Lee 15-10-14
 	{
+		prevLoS.Clear ();
+		marines = game.gameMap.getUnits (Game.EntityType.SM);
 		for (int j = 0; j < marines.Count; j++)
 			prevLoS.Add (marines[j], game.algorithm.findLoS(marines[j]));
 		//resets prevLoS and sets it again
