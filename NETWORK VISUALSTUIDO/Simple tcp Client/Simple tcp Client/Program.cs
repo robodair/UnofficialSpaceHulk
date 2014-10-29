e67b9e1dc0ea2c07ba6494b9a844462536edd7ca
AU-Wildcat <0067059@radford.act.edu.au>
2014-10-29 12:14:26 +1100

    Netwroks

--- NETWORK VISUALSTUIDO/Simple tcp Client/Simple tcp Client/Program.cs
+++ NETWORK VISUALSTUIDO/Simple tcp Client/Simple tcp Client/Program.cs
@@ -17,6 +17,42 @@ namespace Simple_tcp_Client
             string input, stringData;
             IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("172.26.187.8"), 139);
 
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            try
+            {
+
+                server.Connect(ipep);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Unable to connect to the server.");
+                Console.WriteLine(e.ToString());
+
+                return;
+            }
+
+            int recv = server.Receive(data);
+            stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine(stringData);
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "exit")
+                    break;
+                server.Send(Encoding.ASCII.GetBytes(input));
+                data = new byte[1024];
+                recv = server.Receive(data);
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine(stringData);
+
+
+            }
+            Console.WriteLine("Disconnecting from server");
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
 
 
         }
