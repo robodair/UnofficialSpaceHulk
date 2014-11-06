a57f9ab26756470204e04434a8810c3b5aa1a091
Alisdair Robertson <fluffyscarecrow@users.noreply.github.com>
2014-11-06 22:59:33 +1100

    Added Splash Screen And Cursor
    - Added a scene to run first (as a splash screen)
    - Skinned the cursor

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// Splash screen display, moves to the actual game after displaying this image
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	public float timeToDisplayImage; 	// The time to wait
+	public int nextLevelToLoad;			// The level to be loaded next
+	
+	private float timeCount;		//Holding Value
+
+	void Update () {
+		timeCount += Time.deltaTime;
+		if (timeCount >= timeToDisplayImage) {
+			Application.LoadLevel(nextLevelToLoad);
+		}
+	}
+}
